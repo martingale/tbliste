821a3d69d0a72c4b7d1ccf3ad602a8a638522c78
vc-k <velicankupeli@gmail.com>
2016-02-12 15:57:55 +0200

    Version Fri Feb 12 15:57:55 EET 2016

--- tblisteUbdater.sh
+++ tblisteUbdater.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/c1/hrn/tbliste
+cd /home/c1/bondProject/tbliste
 wget --tries 2 -N -o wgetOut 'http://www.borsaistanbul.com/datum/tbliste.zip'
 cfirst="$(echo $?)"
 csecond="$(echo $(grep -c 'Server file no newer' wgetOut))"
