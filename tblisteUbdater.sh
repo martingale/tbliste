e7ed5f0c09d890a9a3e1cb72272a7ea556772cc1
vc-k <velicankupeli@gmail.com>
2016-02-12 09:45:06 +0200

    Fri Feb 12 09:45:06 EET 2016

---
+++ tblisteUbdater.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# wget -N http://www.borsaistanbul.com/datum/tbliste.zip 
+# recentMd5=$(echo $(md5sum tbliste.xls) | grep -oEi '[[:alnum:]]{32}')
+# read -r -d '' lastMd5 < <(cat lastMd5.txt)
+
+# if [ $lastMd5 != $recentMd5 ]; then
+#                echo expression evaluated as false
+#             else
+#                echo expression evaluated as true
+#            fi
+# xls2csv -x -s cp1254 -d utf-8  "tbliste.xls" > tb.csv
+
+# read -r -d '' wgetOut < <(cat wgetOut)
+
+
+wget --tries 2 -N -o wgetOut 'http://www.borsaistanbul.com/datum/tbliste.zip'
+cfirst="$(echo $?)"
+csecond="$(echo $(grep -c 'Server file no newer' wgetOut))"
+# echo $cfirst $csecond
+if [[ "$cfisrt" == "0" && "$csecond" == "0" ]]; then
+	unzip tbliste.zip -d ./
+	in2csv -f xls tbliste.xls > tbliste.csv
+	echo "tbliste.csv updated"
+else
+	echo "No new tbliste.zip file"
+fi
