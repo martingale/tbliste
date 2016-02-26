b6af90d24207091c999ae3303834cce3c9a93795
vc-k <velicankupeli@gmail.com>
2016-02-26 12:22:43 +0200

    Version Fri Feb 26 12:22:43 EET 2016

--- tblisteUbdater.sh
+++ tblisteUbdater.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 cd /home/c1/bondProject/tbliste
-wget --tries=2 --timeout=2 -N -o wgetOut http://www.borsaistanbul.com/datum/tbliste.zip
+wget --tries=2 --timeout=5 -N -o wgetOut http://www.borsaistanbul.com/datum/tbliste.zip
 cfirst="$(echo $?)"
 csecond="$(echo $(grep -c 'Server file no newer' wgetOut))"
  echo $cfirst $csecond
 if [ $csecond -eq 0 ] && [ $cfirst -eq 0 ]; then
 	unzip -o tbliste.zip -d ./
-##  recentMd5=$(echo $(md5sum tbliste.xls) | grep -oEi '[[:alnum:]]{32}')
+       ##  recentMd5=$(echo $(md5sum tbliste.xls) | grep -oEi '[[:alnum:]]{32}')
 	sleep 0
 	in2csv -f xls tbliste.xls > tbliste.csv
-	slep 1
+	slep 0
 	sed -i 's/[^,]*,//' tbliste.csv	# delete 1st column.
 	sed -i '1d;4d' tbliste.csv # delete 1st and 4th rows.
         cat tbliste.csv | egrep -i "^[A-Z0-9]{12,},,.*$" > tbliste.tmp 
