c56896705a439a55b19beeb9d0b5ff733e433a52
vc-k <velicankupeli@gmail.com>
2016-03-02 21:17:21 +0200

    Version Çrş Mar  2 21:17:21 EET 2016

--- tblisteUbdater.sh
+++ tblisteUbdater.sh
@@ -8,7 +8,7 @@ if [ $csecond -eq 0 ] && [ $cfirst -eq 0 ]; then
 	unzip -o tbliste.zip -d ./
        ##  recentMd5=$(echo $(md5sum tbliste.xls) | grep -oEi '[[:alnum:]]{32}')
 	sleep 0
-	ssconvert -O 'separator=;' tbliste.xls tbliste.csv
+	ssconvert -O 'separator=; locale=C' tbliste.xls tbliste.csv
 #	libreoffice --headless --convert-to csv --outdir . *.xls
 #	in2csv -f xls tbliste.xls > tbliste.csv
 	sed -i 's/[^,]*,//' tbliste.csv	# delete 1st column.
