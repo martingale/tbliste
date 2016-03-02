2ceaeddb3fe04d6fd48331a76a46f3421cd995da
vc-k <velicankupeli@gmail.com>
2016-03-02 21:49:08 +0200

    Version Çrş Mar  2 21:49:08 EET 2016

--- tblisteUbdater.sh
+++ tblisteUbdater.sh
@@ -7,7 +7,7 @@ csecond="$(echo $(grep -c 'Server file no newer' wgetOut))"
 if [ $csecond -eq 0 ] && [ $cfirst -eq 0 ]; then
 	unzip -o tbliste.zip -d ./
        ##  recentMd5=$(echo $(md5sum tbliste.xls) | grep -oEi '[[:alnum:]]{32}')
-	sleep 1
+	sleep 0
 	ssconvert -O 'separator=; locale=en_US.UTF-8' tbliste.xls tbliste.txt
 	mv tbliste.txt  tbliste.csv
 #	libreoffice --headless --convert-to csv --outdir . *.xls
