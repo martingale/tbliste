3b4b993b25a09144581ea23b99edc8f0b8c741c5
VeliKupeli <velican.kupeli@matriksdata.com>
2017-01-18 17:13:30 +0300

    Version Wed Jan 18 17:13:30 +03 2017

--- tblisteUbdater.sh
+++ tblisteUbdater.sh
@@ -26,7 +26,6 @@ if [ $csecond -eq 0 ] && [ $cfirst -eq 0 ]; then
         done   
 	grep  -E "[A-Z 0-9]{11}|^ISIN[[:space:]]+[CK]od" tbliste.csv > temp.csv # remove the lines occuring after excel alt+Enter oddies
         mv temp.csv tbliste.csv
-	rm temp.csv 
 	iconv tbliste.csv -f UTF-8 -t WINDOWS-1254//TRANSLIT -o tbliste_WIN1254.csv # finally, convert the encosing 
 	echo "tbliste.csv updated"
 	git add . --all
