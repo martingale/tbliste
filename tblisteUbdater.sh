55802ce4b900bb0a35381c6c4a79b29821b80f49
VeliKupeli <velican.kupeli@matriksdata.com>
2017-02-28 19:05:56 +0300

    Version Tue Feb 28 19:05:56 +03 2017

--- tblisteUbdater.sh
+++ tblisteUbdater.sh
@@ -26,6 +26,14 @@ if [ $csecond -eq 0 ] && [ $cfirst -eq 0 ]; then
         done   
 	 grep  -E "[A-Z 0-9]{11}|^ISIN[[:space:]]+[CK]od" tbliste.csv > temp.csv # remove the lines occuring after excel alt+Enter oddies
          mv temp.csv tbliste.csv
+# Remove the exceptional ISINs so that they shall not be updated
+	for i in $( cat dontUpdateISIN.txt); do
+                var=$(cat tbliste.csv | grep -n "$i" | grep -oEi "^[0-9]{1,3}")
+		if [ "$var" != "" ];then
+			sed -i '$(var)d;' tbliste.csv	
+		fi
+        done
+# End of remove the exceptional ##
 	iconv tbliste.csv -f UTF-8 -t WINDOWS-1254//TRANSLIT -o tbliste_WIN1254.csv # finally, convert the encosing 
 	echo "tbliste.csv updated"
 	cut -d ";" -f 1-29 < tbliste_WIN1254.csv > foo.csv &&  mv foo.csv tbliste_WIN1254.csv # remove the columns after 29th column (@ 2017-02-28)
